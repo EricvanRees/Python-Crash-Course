68df46c209aee38ffe43af17d1408a22903efafa
Eric van Rees <36233467+EricvanRees@users.noreply.github.com>
2018-10-31 17:50:41 +0100

    Add files via upload

---
+++ 1-2.py
@@ -0,0 +1,15 @@
+print("Hello Python world!")
+
+# make a typo somewhere and run the line
+# Can you make sense of the error?
+
+pint("Hello Python world!")
+# generates name error
+
+'''Infinite skills: if you had infinite programming skills,
+what would you build?
+
+Answer: A site such as www.discogs.com, or a wordpress website 
+where I can sell my own books using a payment system.
+Or else, a mapping application that lets you drag-and-drop spreadsheet
+data on a map.
